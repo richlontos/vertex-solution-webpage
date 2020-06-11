7088ac8887c0441e43663e90ad31dd1a194bb481
richard lontos <richard.lontos95@gmail.com>
2020-06-11 09:00:00 -0700

    changes

--- src/pages/HomeServices.jsx
+++ src/pages/HomeServices.jsx
@@ -1,10 +1,10 @@
-import { ImportExport } from "@mui/icons-material"
+// import { ImportExport } from "@mui/icons-material"
 import React from "react"
 import AboutServices from "../components/ServicesPages/AboutServices"
 import Blog from "../components/ServicesPages/Blog"
 import ContactServices from "../components/ServicesPages/ContactService"
-import Counter from "../components/ServicesPages/Counter"
-import Portfolio from "../components/ServicesPages/Portfolio"
+// import Counter from "../components/ServicesPages/Counter"
+// import Portfolio from "../components/ServicesPages/Portfolio"
 import ServicesPage from "../components/ServicesPages/ServicesPage"
 import { Testimonials } from "../components/ServicesPages/Testimonials"
 import '../components/serviceFolder/ServicesTest.css'
