9662e2ed9cdf4ccd9e613f625d52878d1a027f10
richard lontos <richard.lontos95@gmail.com>
2020-11-12 09:00:00 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
 import Home from './pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {  Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Footer } from './components/Footer';
 import Test from './pages/Test';
@@ -19,6 +19,7 @@ import WarehouseServices from './pages/WarehouseServices';
 import VolumesServices from './pages/VolumeServices';
 import './components/serviceFolder/ServicesTest.css'
 import CarriersTab from './pages/CarriersTab';
+import HashLoader from "react-spinners/HashLoader";
 
 import { CursorContext } from './context/CursorContext';
 import { motion } from 'framer-motion';
@@ -32,40 +33,68 @@ import ResourcesTab from './components/Resources/ResourcesTab';
 
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2400)
+  }, [])
+
   const { cursorVariants, cursorBG } = useContext(CursorContext);
- //aos
- useEffect(() => {
-  AOS.init()
-  AOS.refresh()
-}, [])
+  //aos
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh()
+  }, [])
   return (
     <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/test' element={<Test />}></Route>
-        <Route path='/carriers' element={<CarriersTab />}></Route>
-        <Route path='/services' element={<HomeServices />}></Route>
-        <Route path='/servicesLTL' element={<LTLServices />}></Route>
-        <Route path='/careers' element={<Careers />}></Route>
-        <Route path='/contactus' element={<ContactUs />}></Route>
-        <Route path='/ourservices' element={<OurServices />}></Route>
-        <Route path='/RailServices' element={<RailServices />}></Route>
-        <Route path='/AirServices' element={<AirServices />}></Route>
-        <Route path='/OceanServices' element={<OceanServices />}></Route>
-        <Route path='/VolumePartial' element={<VolumesServices />}></Route>
-        <Route path='/WarehouseServices' element={<WarehouseServices />}></Route>
-        <Route path='/offer' element={<WhatOffer />}></Route>
-        <Route path='/faq' element={<Faq />}></Route>
-        <Route path='/resources' element={<Resources />}></Route>
+      {
+        loading ?
 
-      </Routes>
-      <Footer />
-      <motion.div
-        variants={cursorVariants}
-        animate={cursorBG}
-        className='w-[32px] h-[32px] mousePoint  fixed top-0 left-0 pointer-events-none z-50 rounded-full'
-      ></motion.div>
+          <div className='appLoader'>
+
+            <HashLoader
+              color="#ffffff"
+              loading={loading}
+              margin={0}
+              size={100}
+              width={0}
+            />
+          </div>
+
+          :
+
+          <>
+            <NavBar />
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='/test' element={<Test />}></Route>
+              <Route path='/carriers' element={<CarriersTab />}></Route>
+              <Route path='/services' element={<HomeServices />}></Route>
+              <Route path='/servicesLTL' element={<LTLServices />}></Route>
+              <Route path='/careers' element={<Careers />}></Route>
+              <Route path='/contactus' element={<ContactUs />}></Route>
+              <Route path='/ourservices' element={<OurServices />}></Route>
+              <Route path='/RailServices' element={<RailServices />}></Route>
+              <Route path='/AirServices' element={<AirServices />}></Route>
+              <Route path='/OceanServices' element={<OceanServices />}></Route>
+              <Route path='/VolumePartial' element={<VolumesServices />}></Route>
+              <Route path='/WarehouseServices' element={<WarehouseServices />}></Route>
+              <Route path='/offer' element={<WhatOffer />}></Route>
+              <Route path='/faq' element={<Faq />}></Route>
+              <Route path='/resources' element={<Resources />}></Route>
+
+            </Routes>
+            <Footer />
+            <motion.div
+              variants={cursorVariants}
+              animate={cursorBG}
+              className='w-[32px] h-[32px] mousePoint  fixed top-0 left-0 pointer-events-none z-50 rounded-full'
+            ></motion.div>
+          </>
+      }
     </div>
   );
 }
