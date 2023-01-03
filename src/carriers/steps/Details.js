0fc9d25a6f6ea0c53e318cffa4a44b2bbcf5ee5f
richard lontos <richard.lontos95@gmail.com>
2023-01-02 23:42:39 -0800

    changes

--- src/carriers/steps/Details.js
+++ src/carriers/steps/Details.js
@@ -1,3 +1,4 @@
+import { Row, Col } from "react-bootstrap";
 import { useStepperContext } from "../../contexts/StepperContext";
 
 export default function Details() {
@@ -9,35 +10,115 @@ export default function Details() {
   };
   return (
     <div className="flex flex-col ">
+      <Row>
+        <Col>
+        
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Address
+        Company Name
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
-            value={userData["address"] || ""}
-            name="address"
-            placeholder="Address"
+            name="company"
+            placeholder="Company Name"
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
+        </Col>
+        <Col>
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          City
+        Mailing Address
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
-            value={userData["city"] || ""}
-            name="city"
-            placeholder="City"
+            name="mailingAddress"
+            placeholder="Mailing Address"
             type="text"
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
+        
+        </Col>
+      </Row>
+
+
+      <Row>
+        <Col>
+        
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Federal ID #
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="federaiId"
+            placeholder="Federal ID #"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        </Col>
+        <Col>
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        MC or DOT
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="mcdot"
+            placeholder="MC or DOT#"
+            type="text"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        
+        </Col>
+      </Row>
+
+
+      <Row>
+        <Col>
+        
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        City/State/Zip
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="cityStateZip"
+            placeholder="City/State/Zip"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        </Col>
+        <Col>
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Fleet Size
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="fleetSize"
+            placeholder="Fleet Size"
+            type="number"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        
+        </Col>
+      </Row>
     </div>
   );
 }
--- src/carriers/steps/Payment.js
+++ src/carriers/steps/Payment.js
@@ -1,3 +1,4 @@
+import { Col, Row } from "react-bootstrap";
 import { useStepperContext } from "../../contexts/StepperContext";
 
 export default function Payment() {
@@ -9,35 +10,150 @@ export default function Payment() {
   };
   return (
     <div className="flex flex-col ">
+
+      <Row>
+        <Col>
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Credit Card
+          # Of Dry Vans
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
-            value={userData["card"] || ""}
-            name="card"
-            placeholder="Credit Card#"
+            name="dryVan"
+            type="number"
+            placeholder="# Of Dry Vans"
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
+        
+        </Col>
+        <Col>
+        
       <div className="w-full mx-2 flex-1">
         <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
-          Exp
+        # Of Reefers
         </div>
         <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
           <input
             onChange={handleChange}
-            value={userData["exp"] || ""}
-            name="exp"
-            placeholder="YY/MM/DD"
+            name="# Of Reefers"
+            placeholder="# Of Reefers"
+            type="number"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        # Of Flat Beds
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="flatBed"
+            type="number"
+            placeholder="# Of Flat Beds"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        
+        </Col>
+        <Col>
+        
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        # Of Lowboys
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="lowboy"
+            placeholder="# Of Lowboys"
+            type="number"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        # Of Drop Decks
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="dropDeck"
+            type="number"
+            placeholder="# Of Drop Decks"
+            className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
+          />
+        </div>
+      </div>
+        
+        </Col>
+        <Col>
+        
+      <div className="w-full mx-2 flex-1">
+        <div className="font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase">
+        Other
+        </div>
+        <div className="bg-white my-2 p-1 flex border border-gray-200 rounded">
+          <input
+            onChange={handleChange}
+            name="other"
+            placeholder="Other"
             type="text"
             className="p-1 px-2 appearance-none outline-none w-full text-gray-800"
           />
         </div>
       </div>
+        </Col>
+      </Row>
+      <Row>
+            <Col>
+
+              <select
+                style={{ "paddingLeft": "20px" }}
+                name="hazmat"
+                className="pickupAccesorials"
+              >
+                <option value="">Hazmat Endorsed?</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+
+              </select>
+              <select
+                style={{ "paddingLeft": "20px" }}
+                name="satellite"
+                className="pickupAccesorials"
+              >
+                <option value="">Sattelite Equipped?</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+
+              </select>
+              <select
+                style={{ "paddingLeft": "20px" }}
+                name="dropTrailer"
+                className="pickupAccesorials"
+              >
+                <option value="">Ability to Drop Trailers?</option>
+                <option value="yes">Yes</option>
+                <option value="no">No</option>
+
+              </select>
+            </Col>
+          </Row>
     </div>
   );
 }
