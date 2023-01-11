c0cc3e6be5adf1c3faeb286e14adfa77bf62e522
richard lontos <richard.lontos95@gmail.com>
2023-01-10 23:08:14 -0800

    changes

--- src/components/CareersTab.js
+++ src/components/CareersTab.js
@@ -5,19 +5,19 @@ import supportImg from '../assets/support.jpg'
 
 function CareersTab() {
     return (
-        <div name='support' className='w-full mt-24'>
+        <div name='support' className=' w-full mt-24'>
             <div className='w-full h-[700px] bg-gray-900/90 absolute'>
                 <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
             </div>
 
-            <div className='max-w-[1240px] mx-auto text-white relative'>
+            <div className='flex-Wrap max-w-[1240px] mx-auto text-white relative'>
                 <div className='px-4 py-12'>
                     <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Careers</h2>
                     <h3 className='text-5xl font-bold py-6 text-center'>Finding the right Job</h3>
                     <p className='py-4 text-2xl text-slate-300'>The strength of Vertex Soluions will always be our people. By hiring the right people, delivering the right service and doing the right thing, Vertex Solutions has continuously exceeded the expectations of our employees, customers and carriers. It’s what we do together that sets us apart.</p>
                 </div>
 
-                <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+                <div className=' flex flex-row flex-wrap  gap-x-8 gap-y-16 px-20 pt-12 sm:pt-20 text-black'>
                     <div className='bg-white rounded-xl shadow-2xl'>
                         <div className='p-8'>
                             {/* <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]' /> */}
