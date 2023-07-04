da69acb8baf67874ab725933f8b067c07edb5ef1
richard lontos <richard.lontos95@gmail.com>
2023-07-04 15:41:55 -0700

    changes

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-// console.log(process.env.EMAIL_USER);
-// console.log(process.env.EMAIL_PASS);
+// Use the PORT from the environment variables if it's available
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server Running on port ${port}`));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
